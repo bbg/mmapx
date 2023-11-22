b91d0fa650e49ebc0f63ecb5cdc20e752deee536
bbg0x <batuhangoksu@gmail.com>
2023-11-22 12:32:19 +0300

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import million from "million/compiler";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
@@ -13,6 +14,45 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
     splitVendorChunkPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "MMapx",
+        short_name: "MMapx",
+        description: "Mindmap creation board",
+        theme_color: "#ffffff",
+        display: "standalone",
+        lang: "en",
+        scope: "/",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
 });
 
